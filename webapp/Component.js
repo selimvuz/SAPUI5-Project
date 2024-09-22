ad843d253e249861b8922e47d7d4ab4bbffbece1
Yavuz Selim <96388835+selimvuz@users.noreply.github.com>
2024-09-22 19:15:41 +0300

    External projects table added

--- webapp/Component.js
+++ webapp/Component.js
@@ -2,38 +2,39 @@
  * eslint-disable @sap/ui5-jsdocs/no-jsdoc
  */
 
-sap.ui.define([
-        "sap/ui/core/UIComponent",
-        "com/smod/ux/todolist/model/models",
-        "com/smod/ux/todolist/utils/sweetalert"
-    ],
-    function (UIComponent, models, SwalJS) {
-        "use strict";
+sap.ui.define(
+  [
+    "sap/ui/core/UIComponent",
+    "com/smod/ux/todolist/model/models",
+    "com/smod/ux/todolist/utils/sweetalert",
+  ],
+  function (UIComponent, models, SwalJS) {
+    "use strict";
 
-        return UIComponent.extend("com.smod.ux.todolist.Component", {
-            metadata: {
-                manifest: "json"
-            },
+    return UIComponent.extend("com.smod.ux.todolist.Component", {
+      metadata: {
+        manifest: "json",
+      },
 
-            /**
-             * The component is initialized by UI5 automatically during the startup of the app and calls the init method once.
-             * @public
-             * @override
-             */
-            init: function () {
-                // call the base component's init function
-                UIComponent.prototype.init.apply(this, arguments);
-                
-                // enable routing
-                this.getRouter().initialize();
+      /**
+       * The component is initialized by UI5 automatically during the startup of the app and calls the init method once.
+       * @public
+       * @override
+       */
+      init: function () {
+        // call the base component's init function
+        UIComponent.prototype.init.apply(this, arguments);
 
-                // set the device model
-                this.setModel(models.createDeviceModel(), "device");
+        // enable routing
+        this.getRouter().initialize();
 
-                // this.setModel(new sap.ui.model.json.JSONModel({
-                //     bLoadedInitially: false
-                // }), "appModel");
-            }
-        });
-    }
+        // set the device model
+        this.setModel(models.createDeviceModel(), "device");
+
+        // this.setModel(new sap.ui.model.json.JSONModel({
+        //     bLoadedInitially: false
+        // }), "appModel");
+      },
+    });
+  }
 );
